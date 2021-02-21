5a38bb93d0c72490e3f672f2e39711747075391f
Norbert Kehrer <norbert_kehrer@yahoo.de>
2021-02-21 01:19:59 +0100

    Soundtrakker with piano roll

---
+++ news/a_0128.js
@@ -0,0 +1,22 @@
+article( // #128
+  21, 02, 2021,
+  "Soundtrakker Player now has a piano roll display",
+  `
+  <p>
+  I added a piano roll display to my little Soundtrakker player to choose as an alternative
+  to the classic note/instrument/effect display. I also included the possibility to mute each channel individually.
+
+  <p>
+  If you want to try the new version just go to
+  <a href="st_player/st_player.html" target="_blank">Amstrad CPC Soundtrakker Player</a>.
+
+  <p>
+    Here is a screen shot showing the piano roll display:
+  </p>
+  <p>
+    <img src="st_player/soundtrakker_scroll.png" alt="Norbert Kehrer's Amstrad CPC Soundtrakker Player">
+  </p>
+
+          
+  `);
+
